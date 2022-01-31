bd0852211ae1dd6b30076e8d075d6c53ee8a5716
icmor <cornejodlm@ciencias.unam.mx>
2022-01-31 13:39:36 -0600

    Update emacs bookmarks

--- emacs/.emacs.d/var/bookmark-default.el
+++ emacs/.emacs.d/var/bookmark-default.el
@@ -2,74 +2,74 @@
 ;;; This format is meant to be slightly human-readable;
 ;;; nevertheless, you probably don't want to edit it.
 ;;; -*- End Of Bookmark File Format Version Stamp -*-
-(("org-refile-last-stored"
-  (filename . "~/org/gtd/inbox.org")
-  (front-context-string)
-  (rear-context-string)
-  (position . 863))
- ("science"
-  (filename . "~/org/life/science.org")
-  (front-context-string)
-  (rear-context-string)
-  (position . 1))
- ("blocks"
-  (filename . "~/.i3/i3blocks.conf")
-  (front-context-string)
-  (rear-context-string)
-  (position . 1))
- ("i3"
-  (filename . "~/.i3/config")
-  (front-context-string)
-  (rear-context-string)
-  (position . 1))
- ("life"
-  (filename . "~/org/life/")
-  (front-context-string)
-  (rear-context-string)
-  (position . 210))
- ("craft"
-  (filename . "~/org/craft/")
-  (front-context-string)
-  (rear-context-string)
-  (position . 211))
- ("notes"
-  (filename . "~/org/roam/notes/")
-  (front-context-string)
-  (rear-context-string)
-  (position . 213))
- ("fun"
-  (filename . "~/org/lists/fun.org")
-  (front-context-string)
-  (rear-context-string)
-  (position . 1))
- ("projects"
-  (filename . "~/org/gtd/projects.org")
-  (front-context-string)
-  (rear-context-string)
-  (position . 1))
- ("lists"
-  (filename . "~/org/lists/")
-  (front-context-string)
-  (rear-context-string)
-  (position . 213))
- ("inbox"
-  (filename . "~/org/gtd/inbox.org")
-  (front-context-string)
-  (rear-context-string)
-  (position . 1))
- ("habit"
-  (filename . "~/org/gtd/habit.org")
-  (front-context-string)
-  (rear-context-string)
-  (position . 1))
- ("gtd"
-  (filename . "~/org/gtd/gtd.org")
-  (front-context-string)
-  (rear-context-string)
-  (position . 1))
- ("emacs"
-  (filename . "~/.emacs.d/init.el")
-  (front-context-string)
-  (rear-context-string)
-  (position . 1))
- )
+(("purchase"
+ (filename . "~/org/gtd/purchase.org")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("self"
+ (filename . "~/org/life/self.org")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("org-refile-last-stored"
+ (filename . "~/org/gtd/inbox.org")
+ (front-context-string)
+ (rear-context-string)
+ (position . 642))
+("blocks"
+ (filename . "~/.i3/i3blocks.conf")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("i3"
+ (filename . "~/.i3/config")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("craft"
+ (filename . "~/org/craft/")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("notes"
+ (filename . "~/org/roam/notes/")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("fun"
+ (filename . "~/org/lists/fun.org")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("projects"
+ (filename . "~/org/gtd/projects.org")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("lists"
+ (filename . "~/org/lists/")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("inbox"
+ (filename . "~/org/gtd/inbox.org")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("habit"
+ (filename . "~/org/gtd/habit.org")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("gtd"
+ (filename . "~/org/gtd/gtd.org")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+("emacs"
+ (filename . "~/.emacs.d/init.el")
+ (front-context-string)
+ (rear-context-string)
+ (position . 1))
+)
