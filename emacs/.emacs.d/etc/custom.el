fb9f3a60b951f4180118ab5ee1b1e8eea9a3862a
icmor <cornejodlm@ciencias.unam.mx>
2022-02-18 22:12:34 -0600

    transpose-frame

--- emacs/.emacs.d/etc/custom.el
+++ emacs/.emacs.d/etc/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(bash-completion tree-sitter-langs tree-sitter lsp-java lsp-mode avy gcmh magit markdown-mode minions olivetti org-roam org-tree-slide pdf-tools racket-mode speed-type which-key ws-butler)))
+   '(transpose-frame bash-completion tree-sitter-langs tree-sitter avy gcmh magit markdown-mode minions olivetti org-roam org-tree-slide pdf-tools racket-mode speed-type which-key ws-butler)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
