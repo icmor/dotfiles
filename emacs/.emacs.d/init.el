eab6f1bd093a06cb486d429188b3fd35f975afb9
icmor <cornejodlm@ciencias.unam.mx>
2022-02-22 19:03:24 -0600

    encoding

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -338,13 +338,18 @@
 (setq doc-view-resolution 400)
 
 ;;;; Text
-(prefer-coding-system 'utf-8)
 (add-hook 'org-mode-hook #'ws-butler-mode)
 (add-hook 'prog-mode-hook #'ws-butler-mode)
 (setq sentence-end-double-space nil)
 (setq require-final-newline t)
 (setq save-interprogram-paste-before-kill t)
 (setq-default fill-column 80)
+
+;;;;; coding system
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
 
 ;;;; Files
 (setq auto-save-default nil)
