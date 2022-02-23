8a0681624c4aeb54472233482e57551a123e5296
icmor <cornejodlm@ciencias.unam.mx>
2022-02-22 19:02:33 -0600

    pretty format for packages

--- emacs/.emacs.d/etc/custom.el
+++ emacs/.emacs.d/etc/custom.el
@@ -4,7 +4,25 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(transpose-frame bash-completion tree-sitter-langs tree-sitter avy gcmh magit markdown-mode minions olivetti org-roam org-tree-slide pdf-tools racket-mode speed-type which-key ws-butler)))
+   '(avy
+     bash-completion
+     eglot
+     gcmh
+     magit
+     markdown-mode
+     minions
+     olivetti
+     org-roam
+     org-tree-slide
+     pdf-tools
+     racket-mode
+     rfc-mode
+     speed-type
+     transpose-frame
+     tree-sitter
+     tree-sitter-langs
+     which-key
+     ws-butler)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
