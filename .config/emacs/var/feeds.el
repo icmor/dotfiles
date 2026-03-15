c80ddd1e4fefcbc7f4a59ada0cbb57b6bc62fa09
icmor <icornejomora@gmail.com>
2026-03-15 00:08:09 -0600

    minor change

--- .config/emacs/var/feeds.el
+++ .config/emacs/var/feeds.el
@@ -1,13 +1,18 @@
 ;; -*- coding: utf-8-emacs; mode: lisp-data; lexical-binding: t -*-
 (setq
  elfeed-feeds
- '("https://www.youtube.com/feeds/videos.xml?channel_id=UCKpBmnEfSy20al-Fk_2YzVA" ; charalanahzard
-   "https://www.youtube.com/feeds/videos.xml?channel_id=UC2SVUGk_kWw62xkFxOGI7Ig" ; Internet Pitstop
-   "https://www.youtube.com/feeds/videos.xml?channel_id=UC2Kyj04yISmHr1V-UlJz4eg" ; Jared Henderson
-   "https://www.youtube.com/feeds/videos.xml?channel_id=UCshObcm-nLhbu8MY50EZ5Ng" ; Benn Jordan
-   "https://www.youtube.com/feeds/videos.xml?channel_id=UCriil4aussYUAGVwpm_zI9w" ; Sarah Davis Baker
-   "https://www.youtube.com/feeds/videos.xml?channel_id=UCaTznQhurW5AaiYPbhEA-KA" ; Molly Rocket
-   "https://www.youtube.com/feeds/videos.xml?channel_id=UC5CYeHPLer3lbEhgonvbbAA" ; Noah Caldwell-Gervais
-   "https://www.youtube.com/feeds/videos.xml?playlist_id=PLxp4ZFkiTSQJISiUkeyznN47GSE2SeXdG" ; Play, Watch, Listen
-   "https://www.youtube.com/feeds/videos.xml?playlist_id=PLbcyJSeTLtlwyCd8BWh0iFvhJzmn9B6NN" ; Friends Per Second
+ '(
+   ;; video
+   "https://www.youtube.com/feeds/videos.xml?channel_id=UCKpBmnEfSy20al-Fk_2YzVA"		; Charalanahzard
+   "https://www.youtube.com/feeds/videos.xml?channel_id=UC2SVUGk_kWw62xkFxOGI7Ig"		; Internet Pitstop
+   "https://www.youtube.com/feeds/videos.xml?channel_id=UC2Kyj04yISmHr1V-UlJz4eg"		; Jared Henderson
+   "https://www.youtube.com/feeds/videos.xml?channel_id=UCshObcm-nLhbu8MY50EZ5Ng"		; Benn Jordan
+   "https://www.youtube.com/feeds/videos.xml?channel_id=UCriil4aussYUAGVwpm_zI9w"		; Sarah Davis Baker
+   "https://www.youtube.com/feeds/videos.xml?channel_id=UCaTznQhurW5AaiYPbhEA-KA"		; Molly Rocket
+   "https://www.youtube.com/feeds/videos.xml?channel_id=UC5CYeHPLer3lbEhgonvbbAA"		; Noah Caldwell-Gervais
+   ;; podcasts
+   "https://www.youtube.com/feeds/videos.xml?playlist_id=PLxp4ZFkiTSQJISiUkeyznN47GSE2SeXdG"	; Play, Watch, Listen
+   "https://www.youtube.com/feeds/videos.xml?playlist_id=PLbcyJSeTLtlwyCd8BWh0iFvhJzmn9B6NN"	; Friends Per Second
+   ;; software
+   "https://github.com/arkenfox/user.js/releases.atom"						; arkenfox
    ))
