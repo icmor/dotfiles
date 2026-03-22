b1de55863c746345b221fed8e632671f5d4fc14a
icmor <icornejomora@gmail.com>
2026-03-22 13:21:50 -0600

    sorry guys

--- .config/emacs/var/feeds.el
+++ .config/emacs/var/feeds.el
@@ -4,12 +4,10 @@
  '(
    ;; video
    "https://www.youtube.com/feeds/videos.xml?channel_id=UCKpBmnEfSy20al-Fk_2YzVA"		; Charalanahzard
-   "https://www.youtube.com/feeds/videos.xml?channel_id=UC2SVUGk_kWw62xkFxOGI7Ig"		; Internet Pitstop
    "https://www.youtube.com/feeds/videos.xml?channel_id=UC2Kyj04yISmHr1V-UlJz4eg"		; Jared Henderson
    "https://www.youtube.com/feeds/videos.xml?channel_id=UCshObcm-nLhbu8MY50EZ5Ng"		; Benn Jordan
    "https://www.youtube.com/feeds/videos.xml?channel_id=UCriil4aussYUAGVwpm_zI9w"		; Sarah Davis Baker
    "https://www.youtube.com/feeds/videos.xml?channel_id=UCaTznQhurW5AaiYPbhEA-KA"		; Molly Rocket
-   "https://www.youtube.com/feeds/videos.xml?channel_id=UC5CYeHPLer3lbEhgonvbbAA"		; Noah Caldwell-Gervais
    ;; podcasts
    "https://www.youtube.com/feeds/videos.xml?playlist_id=PLxp4ZFkiTSQJISiUkeyznN47GSE2SeXdG"	; Play, Watch, Listen
    "https://www.youtube.com/feeds/videos.xml?playlist_id=PLbcyJSeTLtlwyCd8BWh0iFvhJzmn9B6NN"	; Friends Per Second
